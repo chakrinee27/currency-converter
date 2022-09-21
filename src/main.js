b1e32546137b76b20ad029b96abc03aca749b2ce
Chakrinee <chakrinee99@gmail.com>
2022-09-21 12:53:32 +0530

    refactored code

--- src/main.js
+++ src/main.js
@@ -1,39 +1,3 @@
-// url = "https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}"
-
-// const request = async ( url,{to: to, from: from, amount: amount}, method = 'GET' ) => {
-//     let options = {
-//         method
-//     };
-//     if ( 'GET' == method ) {
-//         url += '?' + ( new URLSearchParams( {to: to, from: from, amount: amount} ) ).toString();
-//     } else {
-//         options.body = JSON.stringify( {to: to, from: from, amount: amount} );
-//     }
-    
-//     const response = await fetch("url", options);
-//     const data = await response.json();
-//        console.log(data);
-// };
-
-// const get = ( url, {to: to, from: from, amount: amount} ) => request( url, {to: to, from: from, amount: amount}, 'GET' );
-
-// get( "url",{to: to, from: from, amount: amount} )
-// .then( response => {
-//     console.log(response)
-// } )
-
-// const url = "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=INR&amount=6500";
-// let options = {
-//     method: 'GET',
-//     headers: {
-//         'apikey':'ksh5KEsMhzI2at2GC0ifMjOwVXkXTrOJ'
-//     }
-// }
-// let fetchRes = fetch(url, options);
-// fetchRes.then(res => res.json()).then(d => {
-//         console.log(d.result);
-//     });
-
 function displayPrice() {
     var apikey = config.API_KEY;
     let amount = document.getElementById('amount').value;
@@ -41,7 +5,7 @@ function displayPrice() {
     let options = {
         method: 'GET',
         headers: {
-            'apikey':apikey
+            'apikey':"ksh5KEsMhzI2at2GC0ifMjOwVXkXTrOJ"
         }
     }
     let fetchRes = fetch(url, options);
