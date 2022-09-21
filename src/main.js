67ccfbdaee8cadc53bfa9314b19b526ff3b70bce
Chakrinee <chakrinee99@gmail.com>
2022-09-21 12:45:43 +0530

    fixed security issues

--- src/main.js
+++ src/main.js
@@ -35,12 +35,13 @@
 //     });
 
 function displayPrice() {
+    var apikey = config.API_KEY;
     let amount = document.getElementById('amount').value;
     const url = "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=INR&amount="+amount;
     let options = {
         method: 'GET',
         headers: {
-            'apikey':'ksh5KEsMhzI2at2GC0ifMjOwVXkXTrOJ'
+            'apikey':apikey
         }
     }
     let fetchRes = fetch(url, options);
